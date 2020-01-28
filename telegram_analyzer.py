b18ae3be2fd74b4569430f0e5684fb62b9796c10
Nazar Sopiha <nazar.sopiha@gmail.com>
2020-01-28 23:28:53 +0100

    more data analyzed

--- telegram_analyzer.py
+++ telegram_analyzer.py
@@ -1,6 +1,7 @@
 import json
 import sys
 from os import path
+from collections import Counter
 
 '''     
     structure of telegram JSON:
@@ -13,17 +14,25 @@ from os import path
 '''
 
 # for opening file
-file_path = path.relpath("telegram_json/result.json")
+file_path = path.relpath("result.json")
 
 # for saving all info about chats
 contacts = {}
 
 # account owner's id
-owner_id = 0
+owner_id = None
 
-chats_to_analyze = 3
+chats_to_analyze = 10
 
-  
+most_popular_words_number = 10
+
+most_popular_symb_number = 10
+
+last_sender = None
+
+last_time_sent = None
+
+
 def get_owner_name(data):
     personal_info = data.get("personal_information")
     if personal_info is None:
@@ -60,7 +69,7 @@ def insertionSort(arr):
         j = i - 1
         while j >= 0 and key > len(get_messages(arr[j])):
             arr[j + 1] = arr[j]
-            j -= 1
+            j -= 1 
         arr[j + 1] = temp
     return arr
 
@@ -132,30 +141,41 @@ def init_contact(id, name):
     contact["symbol_counter"] = 0
     contact["word_counter"] = 0
     contact["avg_len"] = 0
-    contact['links'] = 0
+    contact['comma_ctr'] = 0
+    contact['links_shared'] = 0
+    contact['msg_edited'] = 0
+    contact["words_usage"] = Counter()
+    contact["symb_usage"] = Counter()
     contacts[owner_id][id] = {}
     contacts[owner_id][id]["message_counter"] = 0
     contacts[owner_id][id]["symbol_counter"] = 0
     contacts[owner_id][id]["word_counter"] = 0
     contacts[owner_id][id]["avg_len"] = 0
-    contacts[owner_id][id]["links"] = 0
-    #contact["words per message"] = 0
-    #contact["most common words"] = {}
+    contacts[owner_id][id]["comma_ctr"] = 0
+    contacts[owner_id][id]["links_shared"] = 0
+    contacts[owner_id][id]["msg_edited"] = 0
+    contacts[owner_id][id]["words_usage"] = Counter()
+    contacts[owner_id][id]["symb_usage"] = Counter()
 
 
 def register_contact(messages_list):
     global contacts
     global owner_id
     for i in range(len(messages_list)):
-        temp_id = messages_list[i].get("from_id")
-        if temp_id == owner_id:
-            continue
-        if temp_id not in contacts:
-            name = messages_list[i].get("from")
-            init_contact(temp_id, name)
-            return temp_id
-        else:
-            print("looks like id ", temp_id, "is already registered")
+        if messages_list[i].get("type") == "message":
+            temp_id = messages_list[i].get("from_id")
+            if temp_id is None:
+                print("smth went wrong registering contact, message: ", messages_list[i])
+            if temp_id == owner_id:
+                continue
+            if temp_id not in contacts:
+                name = messages_list[i].get("from")
+                if name is None:
+                    print("smth went wrong registering contact, message: ", messages_list[i])
+                init_contact(temp_id, name)
+                return temp_id
+            else:
+                print("looks like id ", temp_id, "is already registered")
 
 
 def analyze_message(message, sender):
@@ -164,6 +184,42 @@ def analyze_message(message, sender):
     for word in words:
         sender["symbol_counter"] += len(word)
         sender["word_counter"] += 1
+        for symbol in word:
+            if symbol in sender["symb_usage"]:
+                sender["symb_usage"][symbol] += 1
+            else:
+                sender["symb_usage"][symbol] = 1
+        if(word[-1] == ','):
+            sender['comma_ctr'] += 1
+        temp = word.replace(',', '').lower()
+        if temp in sender['words_usage']:
+            sender['words_usage'][temp] += 1
+        else:
+            sender['words_usage'][temp] = 1
+
+def compare_time(date):
+    global last_time_sent
+    if date['day'] == last_time_sent['day']:
+        if date['hour'] == last_time_sent['hour']:
+            pass
+            #minutes
+        else:
+            pass
+
+
+def analyze_time(time_sent, sender):
+    global last_sender
+    time_arr = time_sent.split('T')
+    day = time_arr[0].split('-')
+    minute = time_arr[1].split(':')
+    map(int, day)
+    map(int, minute)
+    date = {}
+    date['year'], date['month'], date['day'], date['hour'], date['minute'] = day[0], day[1], day[2], minute[0], minute[1]
+    print(date)
+    if sender != last_sender:
+        compare_time(date)
+        last_sender = sender
 
 
 def analyze_message_list(message_list, second_id):
@@ -182,18 +238,30 @@ def analyze_message_list(message_list, second_id):
                 sender = contacts[owner_id][second_id]
             else:
                 sender = contacts[sender_id]
+
+            # getting only year. If it is 1970, message was not edited
+            time_edited = message.get('edited')[:4]
+            if time_edited != '1970':
+                sender['msg_edited'] += 1
+
+            time_sent = message.get('date')
+            if time_sent is None:
+                print('smth went wrong while getting date of sending message. Exit..')
+                sys.exit()
+            #analyze_time(time_sent, sender)
+
             text = message.get('text')
             if isinstance(text, str):
                 analyze_message(text, sender)
             elif isinstance(text, dict):
-                print('wow')
+                print("i dunno what's it")
             else:
                 for elem in text:
                     if isinstance(elem, str):
                         analyze_message(elem, sender)
                     elif isinstance(elem, dict):
                         if elem.get('type') == 'link':
-                            sender['links'] += 1
+                            sender['links_shared'] += 1
                         analyze_message(elem.get('text'), sender)
         else:
             pass
@@ -219,10 +287,20 @@ def print_data():
             print('chat with: ', user_data['name'])
             print(owner_name, ":")
             for owner_key, owner_value in owner_data.items():
-                print(owner_key, ": ", owner_value)
-            print(user_data['name'], ":")
+                if owner_key == 'words_usage':
+                    print(most_popular_symb_number, " most popular words: ", owner_value.most_common(most_popular_words_number))
+                elif owner_key == 'symb_usage':
+                    print(most_popular_symb_number, " most popular letters: ", owner_value.most_common(most_popular_symb_number))
+                else:
+                    print(owner_key, ": ", owner_value)
+            print('---- partner ---- ')
             for user_key, user_value in user_data.items():
-                print(user_key, ": ", user_value)
+                if user_key == 'words_usage':
+                    print(most_popular_words_number, " most popular words: ", user_value.most_common(most_popular_words_number))
+                elif user_key == 'symb_usage':
+                    print(most_popular_symb_number, " most popular letters: ", user_value.most_common(most_popular_symb_number))
+                else:
+                    print(user_key, ": ", user_value)
             print()
 
 
@@ -251,11 +329,12 @@ else:
             total_msg_ctr = len(messages_list)
             second_id = register_contact(messages_list)
             analyze_message_list(messages_list, second_id)
-
+        else:
+            print('unexpected chat type: ', chat_type)
 
 finally:
     print()
-    print_data()
+    #print_data()
     print()
     print("\nReading finished. Closing..")
     file.close()
